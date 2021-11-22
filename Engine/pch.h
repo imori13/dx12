d2c0af52160ba12cea4db0009a2a1893883c205f
iMori0013 <iMori0013@gmail.com>
2021-11-22 17:55:48 +0900

    .pch設定

---
+++ Engine/pch.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#define NOMINMAX
+#define NOHELP
+#define NODRAWTEXT
+#define NOGDI
+#define NOBITMAP
+
+#include <windows.h>
+
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#ifdef _DEBUG
+#include <dxgidebug.h>
+#endif
+
+#include <cstdint>
+#include <vector>
+#include <memory>
+#include <string>
