fdf3ad3c129206f7d202e380a99efe6ad1c091f8
iMori0013 <iMori0013@gmail.com>
2021-11-27 18:03:15 +0900

    DepthStencilBufferを追加

---
+++ Engine/DepthStencilBuffer.cpp
@@ -0,0 +1,51 @@
+#include "DepthStencilBuffer.h"
+#include "WinApp.h"
+#include "GraphicsCore.h"
+
+void DepthStencilBuffer::Create(DXGI_FORMAT format, float depthValue, uint8_t stencilValue)
+{
+	// ヒープ設定
+	D3D12_HEAP_PROPERTIES prop = {};
+	prop.Type = D3D12_HEAP_TYPE_DEFAULT;
+	prop.CPUPageProperty = D3D12_CPU_PAGE_PROPERTY_UNKNOWN;
+	prop.MemoryPoolPreference = D3D12_MEMORY_POOL_UNKNOWN;
+	prop.CreationNodeMask = 1;
+	prop.VisibleNodeMask = 1;
+
+	// リソース設定
+	D3D12_RESOURCE_DESC resDesc = {};
+	resDesc.Dimension = D3D12_RESOURCE_DIMENSION_TEXTURE2D;
+	resDesc.Alignment = 0;
+	resDesc.Width = Window::g_Width;
+	resDesc.Height = Window::g_Height;
+	resDesc.DepthOrArraySize = 1;
+	resDesc.MipLevels = 1;
+	resDesc.Format = format;
+	resDesc.SampleDesc.Count = 1;
+	resDesc.SampleDesc.Quality = 0;
+	resDesc.Layout = D3D12_TEXTURE_LAYOUT_UNKNOWN;
+	resDesc.Flags = D3D12_RESOURCE_FLAG_ALLOW_DEPTH_STENCIL;
+
+	// リソースを生成する際の深度ステンシルバッファのクリア値
+	D3D12_CLEAR_VALUE clearValue;
+	clearValue.Format = format;
+	clearValue.DepthStencil.Depth = depthValue;
+	clearValue.DepthStencil.Stencil = stencilValue;
+
+	// 深度バッファの生成
+	auto hr = Graphics::g_pDevice->CreateCommittedResource(
+		&prop,
+		D3D12_HEAP_FLAG_NONE,
+		&resDesc,
+		D3D12_RESOURCE_STATE_DEPTH_WRITE,
+		&clearValue,
+		IID_PPV_ARGS(m_pBuffer.GetAddressOf()));
+	if(FAILED(hr))
+	{ return; }
+
+	// パイプライン用ステート設定
+	m_State.DepthEnable = TRUE;
+	m_State.DepthWriteMask = D3D12_DEPTH_WRITE_MASK_ALL;
+	m_State.DepthFunc = D3D12_COMPARISON_FUNC_LESS_EQUAL;	// 深度テストの比較関係
+	m_State.StencilEnable = FALSE;
+}
---
+++ Engine/DepthStencilBuffer.h
@@ -0,0 +1,22 @@
+#pragma once
+
+class DepthStencilBuffer
+{
+public:
+	~DepthStencilBuffer() { Destroy(); }
+
+	void Create(DXGI_FORMAT format, float depthValue, uint8_t stencilValue);
+	void Destroy()
+	{
+		m_pBuffer.Reset();
+	}
+
+	ID3D12Resource* Get() { return m_pBuffer.Get(); }
+	void SetCpuHandle(D3D12_CPU_DESCRIPTOR_HANDLE handle) { m_CpuHandle = handle; }
+	const D3D12_CPU_DESCRIPTOR_HANDLE& GetCpuHandle() const{ return m_CpuHandle; }
+	const D3D12_DEPTH_STENCIL_VIEW_DESC GetView() const { return D3D12_DEPTH_STENCIL_VIEW_DESC{ m_pBuffer->GetDesc().Format, D3D12_DSV_DIMENSION_TEXTURE2D}; }
+private:
+	ComPtr<ID3D12Resource> m_pBuffer;
+	D3D12_DEPTH_STENCIL_DESC m_State;
+	D3D12_CPU_DESCRIPTOR_HANDLE m_CpuHandle;
+};
