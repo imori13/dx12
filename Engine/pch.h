092b7ac08201ba920076c66a811fc72bfe8fe156
iMori0013 <iMori0013@gmail.com>
2021-12-04 10:58:04 +0900

    DirectXTK12ライブラリを追加

--- Engine/pch.h
+++ Engine/pch.h
@@ -14,10 +14,11 @@
 
 #pragma comment(lib, "d3d12.lib")
 #pragma comment(lib, "dxgi.lib")
-#pragma comment(lib, "d3dcompiler.lib")
 #pragma comment(lib, "dxguid.lib")
+#pragma comment(lib, "d3dcompiler.lib")
+#pragma comment(lib, "DirectXTK12.lib")
 
-#include <gsl/gsl>
+#include <GSL/gsl>
 #include <windows.h>
 
 #include <d3d12.h>
