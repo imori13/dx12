481d6d605d6d501615f21ff51acca3726834f9f9
iMori0013 <iMori0013@gmail.com>
2021-12-09 03:05:01 +0900

    移動平均のアルゴリズムを変更

--- Projects/Engine/DataAverage.cpp
+++ Projects/Engine/DataAverage.cpp
@@ -1,27 +1,35 @@
 #include "DataAverage.h"
+#include "Timer.h"
 
 #include <map>
 
+struct Value
+{
+	double Sum = {};
+	std::list<float> ValueList = {};
+};
+
 namespace
 {
-	std::map<std::wstring, std::list<float>> performanceView;
+	std::map<std::wstring, Value> averageList;
 }
 
-void DataAverage::Set(const std::wstring& name, const float value, const Average average)
+// ˆÚ“®•½‹Ï
+void DataAverage::Set(const std::wstring& name, const float value, const Average limit)
 {
-	auto& list = performanceView[name];
-	list.emplace_back(value);
-	if(list.size() > static_cast<uint32_t>(average))
-	{ list.pop_front(); }
+	auto& pair = averageList[name];
+	pair.Sum += pair.ValueList.emplace_back(value);
+
+	// ŒÃ‚¢ƒf[ƒ^‚ð”jŠü‚·‚é
+	while(pair.ValueList.size() * (Timer::g_FrameTime * 60) > static_cast<uint32_t>(limit))
+	{
+		pair.Sum -= pair.ValueList.front();
+		pair.ValueList.pop_front();
+	}
 }
 
 float DataAverage::Get(const std::wstring& name)
 {
-	const auto& list = performanceView[name];
-	float sum = 0;
-	for(const auto element : list)
-	{
-		sum += element;
-	}
-	return sum / list.size();
+	const auto& pair = averageList[name];
+	return gsl::narrow_cast<float>(pair.Sum / static_cast<double>(pair.ValueList.size()));
 }
